924ad9ab6b0a11ce460a03bf61b07dafad6bb79b
Adam Elfsborg <adaelf1@utbvox.local>
2020-07-03 11:23:01 +0200

    setting upp the classes

---
+++ financials.py
@@ -0,0 +1,36 @@
+class Activities:
+    def __init__(self, activity, amount):
+        self.activity = activity
+        self.amount = amount
+
+    def __int__(self):
+        return self.amount
+
+    def __str__(self):
+        return self.activity
+
+    def subtract_income(self, income):
+        return income - self.amount 
+
+    
+    
+class Food(Activities):
+    def __init__(self, activity, amount):
+        super().__init__(activity, amount)
+
+
+class Bills(Activities):
+    def __init__(self, activity, amount):
+        super().__init__(activity, amount)
+
+
+class Saveings(Activities):
+    def __init__(self, activity, amount):
+        super().__init__(activity, amount)
+
+    
+a = Activities('football', 500)
+
+print(a.subtract_income(1000))
+
+
